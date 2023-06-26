0f91a7c6932ba98ee2a0dbbcbb23f8dd7dbb56b0
Graf_Tonyakula <tonyakat87@gmail.com>
2023-06-26 15:26:56 +0600

    Solve problems

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -5,25 +5,26 @@ public class SquareEquation
     public static double[] Solve(double a, double b, double c)
     {
         double D;
+        double eps = 1e-9;
         double[] answer = new double[]{};
         var massif = new double[]{double.NaN, double.NegativeInfinity, double.PositiveInfinity};
-        if ((a == 0) || (massif.Contains(a)) || (massif.Contains(b)) || (massif.Contains(c)))
+        if ((Math.Abs(a) < eps) || (massif.Contains(a)) || (massif.Contains(b)) || (massif.Contains(c)))
             throw new ArgumentException();
         else
         {
             b = b / a;
             c = c / a;
             D = Math.Pow(b,2) - 4 * c;
-            if ((D < 0) && (Math.Abs(D) >= double.Epsilon))
+            if ((D < -eps))
             {
                 answer = new double[]{};
             }
-            else if ((- double.Epsilon < D) && (D < double.Epsilon))
+            else if ((-eps < D) && (D < eps))
             {
                 var x1 = - (b + Math.Sign(b) * Math.Sqrt(D)) / 2;
                 answer = new double[]{x1};
             }
-            else if (D > 0)
+            else
             {
                 var x1 = - (b + Math.Sign(b) * Math.Sqrt(D)) / 2;
                 var x2 = c / x1;
