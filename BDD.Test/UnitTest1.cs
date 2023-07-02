c6b34c868da213e44143b94b6b1e764c7a64e0da
Graf_Tonyakula <tonyakat87@gmail.com>
2023-07-02 23:11:00 +0600

    BDD and XUnit

--- BDD.Test/UnitTest1.cs
+++ BDD.Test/UnitTest1.cs
@@ -6,10 +6,11 @@ namespace BDD.Test;
 class UnitTest1
 {
     double coef_a, coef_b, coef_c;
-    double[] answer = new double[]{};
+    int precision = 9;
+    double[] actual = new double[]{};
 
     [Given ("Квадратное уравнение с коэффициентами ((.*), (.*), (.*))")]
-    public void SetOfCoefficients1(double a, double b, double c)
+    public void SetOfCoefficients(double a, double b, double c)
     {
         coef_a = a;
         coef_b = b;
@@ -17,10 +18,39 @@ class UnitTest1
     }
 
     [When("вычисляются корни квадратного уравнения")]
-    public void Condition1()
+    public void Condition()
     {
-        answer = SquareEquation.Solve(coef_a, coef_b, coef_c);
+        actual = SquareEquation.Solve(coef_a, coef_b, coef_c);
     }
 
     [Then("квадратное уравнение имеет два корня ((.*), (.*)) кратности один")]
+    public void SolvingTheProblem1(double one_number, double two_number)
+    {
+        double[] expected = {one_number, two_number};
+        Assert.True(expected.Length == actual.Length);
+        for (int i = 0; i < actual.Length; i++)
+        {
+            Assert.Equal(expected[i], actual[i], precision);
+        }
+    }
+
+    [Then("квадратное уравнение имеет один корень 1 кратности два")]
+    public void SolvingTheProblem2()
+    {
+        Assert.Equal(1, actual[0], precision);
+    }
+
+    [Then("множество корней квадратного уравнения пустое")]
+    public void SolvingTheProblem3()
+    {
+        Assert.Empty(actual);
+    }
+
+    [Then("выбрасывается исключение ArgumentException")]
+    public void SolvingTheProblem4()
+    {
+        Assert.Throws<ArgumentException>(() => actual);
+    }
+
+
 }
