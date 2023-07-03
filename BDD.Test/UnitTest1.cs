b659f7c4ea268087662ed01a43d3ab60cf8b1d18
Graf_Tonyakula <tonyakat87@gmail.com>
2023-07-03 23:38:54 +0600

    BDD and XUnit

--- BDD.Test/UnitTest1.cs
+++ BDD.Test/UnitTest1.cs
@@ -10,7 +10,7 @@ class UnitTest1
     double[] actual = new double[]{};
 
     [Given (@"Квадратное уравнение с коэффициентами \((.*), (.*), (.*)\)")]
-    public void SetOfCoefficients(double a, double b, double c)
+    public void КвадратноеУравнениеСКоэффициентами(double a, double b, double c)
     {
         coef_a = a;
         coef_b = b;
@@ -18,7 +18,7 @@ class UnitTest1
     }
 
     [When(@"вычисляются корни квадратного уравнения")]
-    public void Condition()
+    public void ВычисляютсяКорниКвадратногоУравнения()
     {
         actual = SquareEquation.Solve(coef_a, coef_b, coef_c);
     }
@@ -26,7 +26,7 @@ class UnitTest1
     [Then(@"квадратное уравнение имеет два корня \((.*), (.*)\) кратности один")]
          public void ТоКвадратноеУравнениеИмеетДваКорняКратностиОдин(double one_number, double two_number)
          {
-             double[] expected = {one_number, two_number};
+            double[] expected = {one_number, two_number};
             Assert.True(expected.Length == actual.Length);
             for (int i = 0; i < actual.Length; i++)
             {
@@ -36,18 +36,88 @@ class UnitTest1
 
 
     [Then(@"квадратное уравнение имеет один корень 1 кратности два")]
-    public void SolvingTheProblem2()
+    public void ТоКвадратноеУравнениеИмеетОдинКореньКратностиДва()
     {
         Assert.Equal(1, actual[0], precision);
     }
 
     [Then(@"множество корней квадратного уравнения пустое")]
-    public void SolvingTheProblem3()
+    public void ТоМножествоКорнейКвадратногоУравненияПустое()
     {
         Assert.Empty(actual);
     }
 
+    [Given(@"Квадратное уравнение с коэффициентами \(NaN, (.*), (.*)\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеAНеМожетБытьОпределено(double a, double b, double c)
+    {
+        coef_a = double.NaN;
+        coef_b = b;
+        coef_c = c;
+    }
 
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), NaN, (.*)\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеBНеМожетБытьОпределено(double a, double b, double c)
+    {
+        coef_a = a;
+        coef_b = double.NaN;
+        coef_c = c;
+    }
+
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), NaN\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеCНеМожетБытьОпределено(double a, double b, double c)
+    {
+        coef_a = a;
+        coef_b = b;
+        coef_c = double.NaN;
+    }
+
+    [Given(@"Квадратное уравнение с коэффициентами \(Double\.PositiveInfinity, (.*), (.*)\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеAНеМожетБытьПоложительнойБесконечностью(double a, double b, double c)
+    {
+        coef_a = double.PositiveInfinity;
+        coef_b = b;
+        coef_c = c;
+    }
+
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), Double\.PositiveInfinity, (.*)\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеBНеМожетБытьПоложительнойБесконечностью(double a, double b, double c)
+    {
+        coef_a = a;
+        coef_b = double.PositiveInfinity;
+        coef_c = c;
+    }
+
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), Double\.PositiveInfinity\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеCНеМожетБытьПоложительнойБесконечностью(double a, double b, double c)
+    {
+        coef_a = a;
+        coef_b = b;
+        coef_c = double.PositiveInfinity;
+    }
+
+    [Given(@"Квадратное уравнение с коэффициентами \(Double\.NegativeInfinity, (.*), (.*)\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеAНеМожетБытьОтрицательнойБесконечностью(double a, double b, double c)
+    {
+        coef_a = double.NegativeInfinity;
+        coef_b = b;
+        coef_c = c;
+    }
+
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), Double\.NegativeInfinity, (.*)\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеBНеМожетБытьОтрицательнойБесконечностью(double a, double b, double c)
+    {
+        coef_a = a;
+        coef_b = double.NegativeInfinity;
+        coef_c = c;
+    }
+
+    [Given(@"Квадратное уравнение с коэффициентами \((.*), (.*), Double\.NegativeInfinity\)")]
+    public void КвадратноеУравнениеСКоэффициентамиГдеCНеМожетБытьОтрицательнойБесконечностью(double a, double b, double c)
+    {
+        coef_a = a;
+        coef_b = b;
+        coef_c = double.NegativeInfinity;
+    }
 
     [Then(@"выбрасывается исключение ArgumentException")]
     public void SolvingTheProblem4()
