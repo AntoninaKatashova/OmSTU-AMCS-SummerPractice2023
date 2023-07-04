48645553a6c3722f51bb26f6966baf074909584c
Graf_Tonyakula <tonyakat87@gmail.com>
2023-07-04 22:35:16 +0600

    Spacebattle test

---
+++ spacebattle/Battle.cs
@@ -0,0 +1,15 @@
+﻿namespace spacebattle;
+
+public class Battle
+{
+    public static double[] ShuttleMovement(bool ChangeOfPosition, bool InstantaneousSpeed, bool ShuttlePosition,
+    double[] Speed, double[] Position)
+    {
+        if (!ChangeOfPosition || !InstantaneousSpeed || !ShuttlePosition)
+        {
+            throw new Exception();
+        }
+        double[] Result = {Speed[0] + Position[0], Speed[1] + Position[1]};
+        return Result;
+    }
+}
